7c647aa8761bab707db5d49f576be86663b41e2b
sozonome <berserk.nate@gmail.com>
2020-05-02 18:49:34 +0700

    Add SSR

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,22 @@
  */
 
 // You can delete this file if you're not using it
+
+/* eslint-disable react/no-danger */
+import React from 'react'
+import { extractStyles } from 'evergreen-ui'
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  // Get the css and hydration script from Evergreen.
+  const { css, hydrationScript } = extractStyles()
+
+  // Takes an array of components as its first argument which are added to
+  // the headComponents array which is passed to the html.js component.
+  setHeadComponents([
+    // We need a key here for Gatsby to stop complaining.
+    <React.Fragment key="evergreen-ssr">
+      <style id="evergreen-css" dangerouslySetInnerHTML={{ __html: css }} />
+      {hydrationScript}
+    </React.Fragment>,
+  ])
+}
