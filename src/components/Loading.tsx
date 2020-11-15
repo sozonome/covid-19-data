b88142a584583748ddca410591237921c71ebe73
sozonome <berserk.nate@gmail.com>
2020-11-15 21:37:44 +0700

    style(components): fix loading color

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import { Box, CircularProgress } from "@chakra-ui/react";
 const Loading = () => {
   return (
     <Box width="100%" textAlign="center" marginY={4}>
-      <CircularProgress isIndeterminate color="orange" />
+      <CircularProgress isIndeterminate color="orange.400" />
     </Box>
   );
 };
