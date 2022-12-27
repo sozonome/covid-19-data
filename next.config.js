c1a293274d5424c877b151555e6064cf73679be0
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-12-27 16:27:59 +0700

    chore: temporary redirect

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
-const withPWA = require("next-pwa");
+const withPWA = require("next-pwa")({
+  disable: process.env.NODE_ENV === "development",
+  dest: "public",
+  register: true,
+});
 
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
-  pwa: {
-    disable: process.env.NODE_ENV === 'development',
-    dest: "public",
-    register: true,
+  redirects: async () => {
+    return [
+      {
+        source: "/:path*",
+        destination: process.env.BASE_URL_REDIRECT,
+        permanent: false,
+      },
+    ];
   },
 });
